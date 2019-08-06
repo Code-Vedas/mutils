dd50d87ed68986fb37933b59ff70dfd022807530
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2019-08-06 16:19:50 +0530

    v0.1.4

--- lib/mutils/serialization/serialization_core.rb
+++ lib/mutils/serialization/serialization_core.rb
@@ -1,12 +1,14 @@
-module Serialization
-  # Module SerializationCore
-  module SerializationCore
-    extend ActiveSupport::Concern
-    included do
-      class << self
-        attr_accessor :scope,
-                      :method_to_serialize,
-                      :attributes_to_serialize
+module Mutils
+  module Serialization
+    # Module SerializationCore
+    module SerializationCore
+      extend ActiveSupport::Concern
+      included do
+        class << self
+          attr_accessor :scope,
+                        :method_to_serialize,
+                        :attributes_to_serialize
+        end
       end
     end
   end
