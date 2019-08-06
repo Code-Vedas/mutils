96fda1d96cb71e28c89498f92fcce3fab3597330
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2019-08-06 16:19:50 +0530

    v0.1.4

--- lib/mutils/serialization/base_serializer.rb
+++ lib/mutils/serialization/base_serializer.rb
@@ -1,45 +1,47 @@
-module Serialization
-  # BaseSerializer
-  module BaseSerializer
-    extend ActiveSupport::Concern
-    include SerializationCore
+module Mutils
+  module Serialization
+    # BaseSerializer
+    module BaseSerializer
+      extend ActiveSupport::Concern
+      include SerializationCore
 
-    def initialize(object)
-      self.class.scope = object
-    end
-
-    def as_json(options = {})
-      super(options)
-      hash = {}
-      self.class.attributes_to_serialize.keys.each do |f|
-        hash[f] = self.class.scope[self.class.attributes_to_serialize[f]]
-      end if self.class.attributes_to_serialize
-
-      self.class.method_to_serialize.keys.each do |f|
-        hash[f] = send(self.class.method_to_serialize[f])
-      end if self.class.method_to_serialize
-      hash
-    end
-
-    class_methods do
-      def attributes(*attributes_list)
-        self.attributes_to_serialize = {} if self.attributes_to_serialize.nil?
-        attributes_list.each do |attr|
-          attributes_to_serialize[attr] = attr
-        end if attributes_list
+      def initialize(object)
+        self.class.scope = object
       end
 
-      def custom_methods(*method_list)
-        self.method_to_serialize = {} if self.method_to_serialize.nil?
-        method_list.each do |attr|
-          method_to_serialize[attr] = attr
-        end if method_list
-      end
-    end
+      def as_json(options = {})
+        super(options)
+        hash = {}
+        self.class.attributes_to_serialize.keys.each do |f|
+          hash[f] = self.class.scope[self.class.attributes_to_serialize[f]]
+        end if self.class.attributes_to_serialize
 
-    private
-    def scope
-      self.class.scope
+        self.class.method_to_serialize.keys.each do |f|
+          hash[f] = send(self.class.method_to_serialize[f])
+        end if self.class.method_to_serialize
+        hash
+      end
+
+      class_methods do
+        def attributes(*attributes_list)
+          self.attributes_to_serialize = {} if self.attributes_to_serialize.nil?
+          attributes_list.each do |attr|
+            attributes_to_serialize[attr] = attr
+          end if attributes_list
+        end
+
+        def custom_methods(*method_list)
+          self.method_to_serialize = {} if self.method_to_serialize.nil?
+          method_list.each do |attr|
+            method_to_serialize[attr] = attr
+          end if method_list
+        end
+      end
+
+      private
+      def scope
+        self.class.scope
+      end
     end
   end
 end
--- lib/mutils/version.rb
+++ lib/mutils/version.rb
@@ -1,3 +1,3 @@
 module Mutils
-  VERSION = '0.1.3'.freeze
+  VERSION = '0.1.4'.freeze
 end
