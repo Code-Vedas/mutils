26ecd518287fee51570703e2a63b1427d659a4fc
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2019-08-06 16:56:12 +0530

    v0.1.5

--- lib/mutils.rb
+++ lib/mutils.rb
@@ -1,4 +1,7 @@
 require 'mutils/version'
+require 'mutils/concerns/paginate'
+require 'mutils/serialization/serialization_core'
+require 'mutils/serialization/base_serializer'
 
 module Mutils
   class Error < StandardError;
