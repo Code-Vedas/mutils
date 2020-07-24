aa9c28fb8da7fcd1915a189bda22f8421a566eee
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2020-07-25 03:13:38 +0800

    fix(semantic-release): enable semantic-release

---
+++ gemdeploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/.gem
+touch $HOME/.gem/credentials
+chmod 0600 $HOME/.gem/credentials
+printf -- "---\n:rubygems_api_key: ${GEM_HOST_API_KEY}\n" > $HOME/.gem/credentials
+gem build *.gemspec
+gem push *.gem
--- lib/mutils/version.rb
+++ lib/mutils/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Mutils
-  VERSION = '1.0.0'
+  VERSION = File.read("Version").split("\n").first
 end
