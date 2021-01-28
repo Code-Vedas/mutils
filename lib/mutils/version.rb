db61ee7a64df19d24fec632724b79c348e3ae6be
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2021-01-28 22:53:05 +0530

    build: rubocop offences

--- lib/mutils/version.rb
+++ lib/mutils/version.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
+# Version
 module Mutils
-  version_file_path = File.join( File.dirname(__FILE__), '../../Version')
+  version_file_path = File.join(File.dirname(__FILE__), '../../Version')
   VERSION = File.read(version_file_path).split("\n").first.gsub('v', '')
 end
